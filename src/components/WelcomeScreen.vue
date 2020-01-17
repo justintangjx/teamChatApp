008d3bee2a2eab80e0de1691d88d14b85919714d
justin tang <justintang@justins-mbp.sg.ibm.com>
2020-01-17 17:09:38 +0800

    route guards - need to enter name

--- src/components/WelcomeScreen.vue
+++ src/components/WelcomeScreen.vue
@@ -32,7 +32,7 @@ export default {
   methods: {
     enterChat() {
       if(this.name) {
-        this.$router.push({ name:'Chat'})
+        this.$router.push({ name:'Chat', params: { name: this.name}})
       } else {
         this.feedback = "you must enter a name to join!"
       }
--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,16 @@ export default new Router({
         {
             path: '/chat',
             name: 'Chat',
-            component: Chat
+            component: Chat,
+            props: true,
+            beforeEnter: (to, from, next) => {
+                if(to.params.name) {
+                    next()
+                } else {
+                    next({ name: 'Welcome' })
+                }
+
+            } 
         }
     ]
 })
